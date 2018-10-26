0c0726bc63904ec66e563be5def4826e2f65a8e4
Jeff <33599649+Fu-Jie@users.noreply.github.com>
2018-10-26 13:27:48 +0800

    Add files via upload

---
+++ syncoh-my-zsh.sh
@@ -0,0 +1,13 @@
+# @Author: fujie
+# @Date:   2018-10-21 11:44:51
+# @Last Modified by:   fujie
+# @Last Modified time: 2018-10-21 12:58:25
+mkdir sh && cd sh
+git clone git@github.com:dygck/oh-my-zsh.git --bare
+cd oh-my-zsh.git
+git push code:bszqw/oh-my-zsh.git --all
+git push code:bszqw/oh-my-zsh.git --tag
+
+
+
+# https://qcloud.coding.net/u/bszqw/p/oh-my-zsh/git/raw/master/tools/install.sh
