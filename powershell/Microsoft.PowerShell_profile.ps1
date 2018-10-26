69292f44be5b4ec891c09aa14e78c89b34ebbf8c
Jeff <33599649+Fu-Jie@users.noreply.github.com>
2018-10-26 13:16:27 +0800

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,125 +1,244 @@
-# chcp 65001
- 
-#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-# Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-# install-Module DirColors
-# install-Module posh-git
-# install-Module oh-my-posh
-# install-Module Screenfetch
-# Import-Module PScolor
-import-module DirColors
+
+
+
+chcp 65001
+
+#--------Install And Import Module-----------------------------------#
+
+#Set-Executionpolicy -ExecutionPolicy Unrestricted
+# Set-Psrepository -Name PSGallery -InstallationPolicy Trusted
+# Install-Module DirColors
+# Install-Module Posh-Git
+# Install-Module Oh-My-posh
+# Install-Module Screenfetch
+# Install-Module Jump.Location
+# Install-Module Pscx -AllowClobber
 # Install-Module -Name PowerShellGet -Force
 # Install-Module -Name PSReadLine -AllowPrerelease -Force
-Import-Module posh-git
-Import-Module oh-my-posh
-# Set-Theme PowerLine
+
+
+Import-Module Pscx -arg "C:\Windows\System32\WindowsPowerShell\v1.0\Pscx.UserPreferences.ps1"
+Import-Module DirColors
+Import-Module Jump.Location
+Import-Module Posh-Git
+Import-Module Oh-My-Posh
+#--------------------------------------------------------------------#
+
+#---------Appearance Settings----------------------------------------#
+Set-Theme PowerLine
 # Set-Theme Fish
 # Set-Theme Agnoster #和powerline一样
 # Set-Theme Paradox #还可以
-Set-Theme Honukai #还可以
+# Set-Theme Honukai #还可以
 # Set-Theme Darkblood #不一样
 # Set-Theme Avit #不一样
-#Set-Theme Sorin
- # Set-Theme tehrob
-Update-DirColors ~/dir_colors
+# Set-Theme Sorin
+# Set-Theme tehrob
+Update-Dircolors ~/dir_colors
 # Screenfetch
+#--------------------------------------------------------------------#
+
+
+# 环境变量
 $desktop="$home/desktop"
-cd $desktop
 $app="C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
+$apps="C:\Users\fj\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
+# $program="E:\Program Files"
+# $program86="E:\Program Files (x86)"
 $fonts="C:\Windows\Fonts"
 $hosts="C:\Windows\System32\drivers\etc\hosts"
-# $tim="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\腾讯软件\TIM\"
-set-alias  sa  set-alias 
-#设置默认打开方式
-sa open invoke-item 
-#设置新建项目
-sa touch new-item
+$posts="C:\Users\fj\Desktop\github\infinite-knowledge.github.io\_posts"
+# $regPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+cd $desktop
+
+#------------------------ Difine Aliases-----------------------------#
+Set-Alias  sa  Set-Alias
+sa open  Invoke-Item  #执行文件默认操作
+sa touch New-Item     #新建文件
+sa st    subl
+sa say   Out-Speech
+sa di    dict
+# modify system aliases to suit individual habits
+Remove-Item -Path "Alias:\clc" -Force
+Remove-Item -Path "Alias:\cls" -Force
+Remove-Item -Path "Alias:\curl" -Force
+Remove-Item -Path "Alias:\cat" -Force
+sa clc Clear-Host
+sa wh Write-Host
+sa of Out-File
+# sa cat `Get-Content  -encoding utf8`
+#--------------------------------------------------------------------#
+function cat{
+
+get-content $args -encoding utf8
+
+
+}
+function Open-App{
+    foreach ($item in $args) {
+        $path=-join($app,"\",$item,".lnk")
+        $path1=-join($apps,"\",$item,".lnk")
+
+            if (test-path $path ) {
+                open $path
+            }
+            elseif (test-path $path1) {
+                open $path1
+            }
+            else {
+                $fullnames=Get-ChildItem $app,$apps -Recurse -Attributes !D|Foreach-Object fullname
+                $basenames=$fullnames|Foreach-Object {Split-Path -Leaf $_}|Foreach-Object {$_.split(".")[0]}
+
+                if ($item -in $basenames) {
+                    $count=$fullnames.count
+                     for ($i=1;$i -lt $count;$i++){
+                            if ( $item -eq $basenames[$i] ) {
+                                open $fullnames[$i]
+                                break
+                            }
+                        }
+                }
+                else {
+                     Write-Host  -ForegroundColor DarkRed "Not Found Application !"
+                }
+
+            }
+    }
+}
+sa op Open-App
+
+Function Set-Profile{
+    code $profile.AllUsersCurrentHost
+}
+sa vsp Code-Profile
 Function Open-Fonts
 {
     open $fonts
 }
-# Function Open-Tim
-# {
-#     open "${tim}Tim.lnk"
-# }
 sa font Open-Fonts
-sa word winword
-sa -name st -value subl
-# $SCRIPTPATH = "C:\Program Files (x86)\Vim"
-# $VIMPATH    = $SCRIPTPATH + "\vim81\vim.exe"
- 
-# sa vi   $VIMPATH  
-# sa vim  $VIMPATH
- 
-# for editing your PowerShell profile
-Function Edit-Profile{
-    st $profile.AllUsersCurrentHost
-}
+
+# create shortcut and  set up command alias
+# function New-Shortcut($TargetPath,$link){
+# $sh = New-Object -ComObject wscript.shell
+# $sc = $sh.CreateShortcut($app + "\" + $link + ".lnk")
+# $sc.targetpath = $TargetPath
+# $sc.save()
+# }
+# sa ln New-Shortcut
+
+
+
+# for editing your Hosts profile and set up command alias
 Function Edit-Hosts{
     st $hosts
 
 }
 sa eh Edit-Hosts
- 
-# for editing your Vim settings
-# Function Edit-Vimrc
-# {
-    # st $home\_vimrc
-# }
 
-sa ep Edit-Profile
-
-# sa ev Edit-Vimrc
- # sa=set-alias
-
-          function Get-CmdletAlias ($cmdletname)
-           {
-              get-alias | Where {$_.definition -like "*$cmdletname*"} | ft Definition, Name -auto
-           }
-
-sa gca  Get-CmdletAlias
+# get all alias according to your command and set up alias
+function Get-Cmdletalias ($cmdletname){
+Get-Alias | Where-Object {$_.definition -like "*$cmdletname*"} | Format-Table Definition, Name -auto
+}
+sa gca  Get-Cmdletalias
 
 
 
-rm -Path "Alias:\clc" -Force
-rm -Path "Alias:\cls" -Force
-rm -Path "Alias:\curl" -Force
-sa clc Clear-host
-sa wh write-host
-sa of Out-File
-function cd... { Set-Location ..\.. }
-function cd.... { Set-Location ..\..\.. }
-function cd..... { Set-Location ..\..\..\.. }
-function cd...... { Set-Location ..\..\..\..\.. }
-function cd....... { Set-Location ..\..\..\..\..\.. }
-function cd........ { Set-Location ..\..\..\..\..\..\.. }
-function cd......... { Set-Location ..\..\..\..\..\..\..\.. }
+
+function .. { Set-Location ..\.. }
+function ... { Set-Location ..\..\.. }
+function .... { Set-Location ..\..\..\.. }
+function ..... { Set-Location ..\..\..\..\.. }
+function ...... { Set-Location ..\..\..\..\..\.. }
+function ....... { Set-Location ..\..\..\..\..\..\.. }
+function ........ { Set-Location ..\..\..\..\..\..\..\.. }
+function ~{ Set-Location ~ }
+function Desk{ Set-Location $desktop }
+
+
+# finding binary executable program from the system search path
 function which
 {
-    $results =New-Object System.Collections.Generic.List[System.Object];
+    $results =New-Object System.Collections.Generic.List[System.Object]
     foreach ($command in $args)
     {
         $path = (Get-Command $command).Source
         if ($path)
         {
-            $results.Add($path);
+            $results.Add($path)
         }
     }
-    return $results;
+    return $results
 }
+
 function cu {
-    curl -x   socks5h://localhost:1080 $args
+    Invoke-WebRequest -x   socks5h://localhost:1080 $args
 }
+
+# set up startup MATLAB options
 function Start-Matlab{
-    matlab -nosplash -sd "E:\f\OneDrive\document\matlab"#启动matlab不显示启动标志，并设置工作目录
+    matlab -nosplash -sd "E:\onedrive\document\matlab"#启动matlab不显示启动标志，并设置工作目录
 }
-sa sm start-Matlab
+
+function Min-Matlab{matlab -automation}
+
+sa sm Start-Matlab
+sa mb Min-Matlab
+
+    Function Compare-ObjectProperties {
+    Param(
+        [PSObject]$ReferenceObject,
+        [PSObject]$DifferenceObject
+    )
+    $objprops = Get-Member -InputObject $ReferenceObject  -MemberType Property, NoteProperty| Foreach-Object Name
+    $objprops +=Get-Member  -InputObject $DifferenceObject  -MemberType Property, NoteProperty | Foreach-Object Name
+    $objprops = $objprops | Sort-Object | Select-Object -Unique
+    $diffs = @()
+    foreach ($objprop in $objprops) {
+        $diff = Compare-Object $ReferenceObject $DifferenceObject -Property $objprop
+        if ($diff) {
+            $diffprops = @{
+                PropertyName = $objprop
+                RefValue     = ($diff | Where-Object {$_.SideIndicator -eq '<='} | Foreach-Object $($objprop))
+                DiffValue    = ($diff | Where-Object {$_.SideIndicator -eq '=>'} | Foreach-Object $($objprop))
+            }
+            $diffs += New-Object PSObject -Property $diffprops
+        }
+    }
+    if ($diffs) {return ($diffs | Select-Object PropertyName, RefValue, DiffValue)}
+}
+
+function Read-Translate {
+
+
+    param (
+
+
+        [string]$sentence,
+        [switch]$word
+
+
+    )
+    if ($word) {
+        $words = $sentence.Split(" ")
+        $strarray=@(".",",","~","``","!","@","#","^",":"," ","?")
+        foreach ($item in $words) {
+            if ($item -notin $strarray) {
+                di $item ; say $item
+            }
+
+        }
+    }
+    else {
+
+        di $sentence  ; say $sentence
+
+
+    }
 
 
 
-
-
-
+}
+sa rat Read-Translate
 
 
 
